1cfd2e3153c95433d53bf3a38547136fcf41c1a3
unknown <web3eva@gmail.com>
2022-11-30 20:00:24 +0100

    track

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,19 @@ export default function Document() {
       lang="en"
       dir="ltr">
         <link rel="shortcut icon" href="/assets/favicon.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YFMKYBDJBC"></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YFMKYBDJBC', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       <Head />
       <body
         style={{
